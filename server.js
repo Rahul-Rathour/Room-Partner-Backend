97926646f1faf0f0816a8a4925647eb35f143729
Rahul_Rathour <rahulrathor0988@gmail.com>
2025-07-12 00:46:37 +0530

    second

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // backend/server.js or backend/index.js
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -7,10 +8,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/roomPartner', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
+
 });
+
 
 const roomPartnerRoutes = require('./routes/roomPartnerRoutes');
 app.use('/api/partner', roomPartnerRoutes);
@@ -18,7 +21,7 @@ app.use('/api/partner', roomPartnerRoutes);
 app.use('/api', require("./routes/userRoutes"));
 app.use('/api/listings', require("./routes/DisplayListingsRoutes"));
 
-
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
   console.log("Server running on port 5000");
 });
