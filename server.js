ebaed23e3a6e1922c692aa2e6c0a0daee37bf370
Rahul <146958652+Rahul-Rathour@users.noreply.github.com>
2025-07-12 11:18:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "https://room-partner-frontend.vercel.app",  // Allow frontend domain
+  methods: ["GET", "POST", "PUT", "DELETE"],            // Add any methods you use
+  credentials: true                                      // If you use cookies/auth
+}));
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
